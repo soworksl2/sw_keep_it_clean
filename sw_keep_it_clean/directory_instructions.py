6e205c80c7b44e8cfebfc809e5db3300381989d0
jimy.waner11@gmail.com <jimy.waner11@gmail.com>
2023-05-28 19:21:42 -0400

    refactoring directory_instructions to improve readability

--- sw_keep_it_clean/directory_instructions.py
+++ sw_keep_it_clean/directory_instructions.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 
+from error_handling import exit_with_error
+
+ABS_PATH_KEY = 'abs_path'
+EXCLUSION_ITEMS_KEY = 'exclusion_items'
+LIFE_TIME_KEY = 'life_time'
 
 @dataclass
 class DirectoryInstructions:
@@ -9,15 +14,14 @@ class DirectoryInstructions:
 
     @staticmethod
     def from_json_dict(json_dict: dict) -> "DirectoryInstructions":
-        abs_path = json_dict['abs_path']
-        exclusion_items = json_dict.get('exclusion_items', [])
+        abs_path = json_dict[ABS_PATH_KEY]
+        exclusion_items = json_dict.get(EXCLUSION_ITEMS_KEY, [])
+        life_time = json_dict.get(LIFE_TIME_KEY, 30)
 
         if not isinstance(exclusion_items, list):
-            raise Exception('the configuration file is bad formatted')
-
-        life_time = json_dict.get('life_time', 30)
+            exit_with_error(f'the {EXCLUSION_ITEMS_KEY} field is not a list')
 
         if not isinstance(life_time, int):
-            raise Exception('the configuration file is bad formatted')
+            exit_with_error(f'the {LIFE_TIME_KEY} field is not an integer')
         
         return DirectoryInstructions(abs_path, exclusion_items, life_time)
